7539c8a5154ea8bf3009c1b16bcc315a83c40f06
Claudia Yepes <claudia.yepes@3ysolutions.com.co>
2023-06-04 20:38:19 -0500

    ajustes

--- src/assets/main.js
+++ src/assets/main.js
@@ -22,16 +22,13 @@ async function fetchData(urlApi){
         let view = `
         ${videos.items.map(video =>`
           <div class="group relative">
-            <div
-              class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-              <a href="https://www.youtube.com/watch?v=${video.id.videoId}">
+            <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
                 <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
-              </a>
             </div>
             <div class="mt-4 flex justify-between">
               <h3 class="text-sm text-gray-700">
                 <span aria-hidden="true" class="absolute inset-0"></span>
-                ${video.snippet.title}
+                <a href="https://www.youtube.com/watch?v=${video.id.videoId}">${video.snippet.title}</a>
               </h3>
             </div>
           </div> 
